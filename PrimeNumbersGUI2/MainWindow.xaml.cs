8acd2f8c6e9d0fed1f8bbe15662e55799408c19a
Tomasz Kisielewski <tomek.kisielewski@gmail.com>
2017-04-17 03:46:13 +0200

    v1

---
+++ PrimeNumbersGUI2/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using PrimeNumbersGUI2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrimeNumbersGUI2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        PrimeNumber primeNumber = new PrimeNumber();
+
+		public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+		private async void Button_Click(object sender, EventArgs e)
+		{
+			int input = Int32.Parse(TextBox.Text);
+			Label.Content = $"Podałeś {input}. Liczę... Chwileczkę";
+			var progress = new Progress<string>(s => Label.Content = s);
+			await Task.Factory.StartNew(() => primeNumber.Find(input),
+										TaskCreationOptions.LongRunning);
+			Label.Content = $"Liczbą pierwszą {primeNumber.GetKey()} z kolei jest {primeNumber.GetValue()}";
+		}
+    }
+}
---
+++ PrimeNumbersGUI2/Models/PrimeNumber.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbersGUI2.Models
+{
+    public class PrimeNumber
+    {
+        int keyPrimeNumber = 1;
+        int valuePrimeNumber = 2;
+
+        public int GetValue()
+        {
+            return valuePrimeNumber;
+        }
+
+        public int GetKey()
+        {
+            return keyPrimeNumber;
+        }
+
+        public void Find(int endKeyPrimeNumber)
+        {
+            if (endKeyPrimeNumber < keyPrimeNumber)
+            {
+                keyPrimeNumber = 1;
+                valuePrimeNumber = 2;
+            }
+        
+            while (endKeyPrimeNumber != keyPrimeNumber)
+            {
+                FindNextPrime(valuePrimeNumber);
+            }
+        }
+
+        public void FindNextPrime(int actualPrimeNumber)
+        {
+            int integear = actualPrimeNumber + 1;
+            while (true)
+            {
+                if (IsPrime(integear))
+                {
+                    keyPrimeNumber++;
+                    valuePrimeNumber = integear;
+                    break;
+                }
+
+                integear++;
+            }
+        }
+
+        public bool IsPrime(int inputInteger)
+        {
+            int divisors = 0;
+
+            for (int i = 1; i <= Math.Sqrt(inputInteger); i++)
+            {
+                if (inputInteger % i == 0)
+                    divisors++;
+
+                if (divisors > 1)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
